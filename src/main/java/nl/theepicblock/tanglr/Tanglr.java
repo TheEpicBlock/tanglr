322622bc337895061923d18e4d2a82d27f59779f
TheEpicBlock <git.teb@theepicblock.nl>
2024-10-21 20:45:03 +0200

    Fix issue with not listening to anything

--- src/main/java/nl/theepicblock/tanglr/Tanglr.java
+++ src/main/java/nl/theepicblock/tanglr/Tanglr.java
@@ -59,7 +59,7 @@ public class Tanglr {
         // Register ourselves for server and other game events we are interested in.
         // Note that this is necessary if and only if we want *this* class (Tanglr) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
-        NeoForge.EVENT_BUS.register(this);
+//        NeoForge.EVENT_BUS.register(this);
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
