20764a596cd74647c73af839a111e569c68a6c19
TheEpicBlock <git.teb@theepicblock.nl>
2024-11-08 22:31:58 +0100

    Cache level conversions

--- src/main/java/nl/theepicblock/tanglr/level/LevelManager.java
+++ src/main/java/nl/theepicblock/tanglr/level/LevelManager.java
@@ -7,13 +7,17 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
 import nl.theepicblock.tanglr.Tanglr;
 
+import java.lang.ref.WeakReference;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.WeakHashMap;
 
 /**
  * Manages the future dimensions of all levels
  */
 public class LevelManager {
+    private static final WeakHashMap<ServerLevel,WeakReference<ServerLevel>> toFutureCache = new WeakHashMap<>();
+
     public static Map<ResourceKey<Level>,ServerLevel> createFutureLevels(Map<ResourceKey<Level>,ServerLevel> levels, ServerParameters p) {
         var futureLevels = new HashMap<ResourceKey<Level>,ServerLevel>();
 
@@ -36,8 +40,21 @@ public class LevelManager {
     }
 
     public static ServerLevel toFuture(ServerLevel present) {
-        var futureKey = toFuture(present.dimension());
-        return present.getServer().getLevel(futureKey);
+        var ref = toFutureCache.computeIfAbsent(present, p -> {
+            var futureKey = toFuture(p.dimension());
+            var futureLevel = p.getServer().getLevel(futureKey);
+            if (futureLevel == null) {
+                return null;
+            } else {
+                return new WeakReference<>(futureLevel);
+            }
+        });
+        if (ref == null) return null;
+        var future = ref.get();
+        if (future == null) {
+            toFutureCache.remove(present);
+        }
+        return future;
     }
 
     public static ResourceKey<Level> toPresent(ResourceKey<Level> future) {
